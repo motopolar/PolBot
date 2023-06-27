ffb1b83fc3d0ba678cae215080c14f1208dc065d
motopolar <120253012+motopolar@users.noreply.github.com>
2023-06-27 01:12:06 -0700

    PolBot

--- plugins/fun-formarpareja.js
+++ plugins/fun-formarpareja.js
@@ -8,7 +8,7 @@ while (b === a)
 m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 𝙲𝙰𝚂𝙰𝚁𝚃𝙴 💍 𝙲𝙾𝙽 ${toM(b)}, 𝙷𝙰𝙲𝙴𝙽 𝚄𝙽𝙰 𝙱𝚄𝙴𝙽𝙰 𝙿𝙰𝚁𝙴𝙹𝙰 💓*
 
 
-*${toM(a)}, YOU SHOULD GET MARRIED💍WITH  ${toM(b)}, THEY MAKE A GOOD COUPLE 💓*`, null, {
+*Jum...*`, null, {
 mentions: [a, b]
 })}
 handler.help = ['formarpareja']
