1417d2b10d1306414f53df6934f758e6ae349cd1
motopolar <120253012+motopolar@users.noreply.github.com>
2023-06-27 02:35:54 -0700

    PolBot

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -1,19 +1,19 @@
-let handler = async (m, {usedPrefix}) => {	
+Coilet handler = async (m, {usedPrefix}) => {	
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
-m.reply(`╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
+m.reply(`╭「➻❥PolBot➻❥」
 │💰 *𝐁𝐀𝐋𝐀𝐍𝐂𝐄* 
 │┈┈┈┈┈┈┈┈┈┈┈┈┈
 │➯‣❏ *✨*𝙽𝚘𝚖𝚋𝚛𝚎:* ${name}
 │➯‣❏ *💎𝙳𝚒𝚊𝚖𝚊𝚗𝚝𝚎𝚜:* ${global.db.data.users[who].limit}💎
 │➯‣❏ *💥Tokens* 🪙${global.db.data.users[who].joincount}
-│➯‣❏ *♦️ʜᴀᴅᴇs-ᴄᴏɪɴs* ${global.db.data.users[who].money}
+│➯‣❏ *♦️Moto Coins* ${global.db.data.users[who].money}
 │➯‣❏ *🧿Exp* ${global.db.data.users[who].exp}
 ╰───────────────╯
 🍁᭢━━━━━━━━━᭥🍁᭢
-╭「➻❥𝙷𝙰𝙳𝙴𝚂-𝙱𝙾𝚃-𝙾𝙼𝙴𝙶𝙰➻❥」
+╭「➻❥PolBot➻❥」
 │➯‣ 📝*𝙽𝙾𝚃𝙰:*
 │➯‣ *para ir al tienda y 𝚌𝚘𝚖𝚙𝚛𝚊𝚛 𝚍𝚒𝚊𝚖𝚊𝚗𝚝𝚎𝚜 💎 usa el  𝚌𝚘𝚖𝚊𝚗𝚍𝚘*
 │➯‣❏ *.shop*
