6d829ff606b165d171c49b831db9717762b51a34
motopolar <120253012+motopolar@users.noreply.github.com>
2023-06-27 01:27:36 -0700

    PolBot

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽𝙰 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽 𝙾 𝚄𝙽𝙰 𝙾𝚁𝙳𝙴𝙽 𝙿𝙰𝚁𝙰 𝚄𝚂𝙰𝚁 𝙻𝙰 𝙵𝚄𝙽𝙲𝙸𝙾𝙽 𝙳𝙴 𝙲𝙷𝙰𝚃𝙶𝙿𝚃*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾𝚂 𝙳𝙴 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽𝙴𝚂 𝚈 𝙾𝚁𝙳𝙴𝙽𝙴𝚂*\n*◉ *${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*◉ *${usedPrefix + command} Codigo en JS para un juego de cartas*\n\n\n[❗] *ENTER A REQUEST OR A COMMAND FOR THE CHATGPT FUNCTION* \n\n *EXAMPLE OF* *PREFERENCE AND* *COMMANDS*\n/${usedPrefix + command} *Reflection on the Merlina 2022 series on netflix*\n*◉ *${usedPrefix + command} JS code for a card game*`
+if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽𝙰 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽 𝙾 𝚄𝙽𝙰 𝙾𝚁𝙳𝙴𝙽 𝙿𝙰𝚁𝙰 𝚄𝚂𝙰𝚁 𝙻𝙰 𝙵𝚄𝙽𝙲𝙸𝙾𝙽 𝙳𝙴 𝙲𝙷𝙰𝚃𝙶𝙿𝚃*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾𝚂 𝙳𝙴 𝙿𝙴𝚃𝙸𝙲𝙸𝙾𝙽𝙴𝚂 𝚈 𝙾𝚁𝙳𝙴𝙽𝙴𝚂*\n*◉ *${usedPrefix + command} ¿Quién es Rosalía?*\n*◉ *${usedPrefix + command} Codigo en JS para un juego de cartas*\n\n\n[❗] *ENTER A REQUEST OR A COMMAND FOR THE CHATGPT FUNCTION* \n\n *EXAMPLE OF* *PREFERENCE AND* *COMMANDS*\n/${usedPrefix + command} *Reflection on the Merlina 2022 series on netflix*\n*◉ *${usedPrefix + command} JS code for a card game*`
 try {
 let IA2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)  
 let IAR2 = await IA2.json()
