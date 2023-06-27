c25ed1ef1c48d0bfd3bc822aea693a3ba9c1c75e
motopolar <120253012+motopolar@users.noreply.github.com>
2023-06-27 01:21:16 -0700

    PolBot

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, args }) => {
 let group = m.chat
 conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
+title: 'PolBot',
 body: '⫷᭄ʜᴀᴅᴇs-ʙᴏᴛ-ᴍᴅ﹏✍',         
 previewType: 0, thumbnail: fs.readFileSync("./views/Menu.jpg"),
-sourceUrl: `https://github.com/OFC-YOVANI/HADES-BOT-MD.git`}}})   
+sourceUrl: `https://github.com/motopolar/PolBot.git`}}})   
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
