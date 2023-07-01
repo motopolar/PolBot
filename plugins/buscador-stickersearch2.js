9c1ebbd7bd5e75da05e7580d5ee8ef5eeb6d5af1
motopolar <120253012+motopolar@users.noreply.github.com>
2023-07-01 01:14:13 -0700

    PolBot

--- plugins/buscador-stickersearch2.js
+++ plugins/buscador-stickersearch2.js
@@ -14,6 +14,5 @@ await conn.sendFile(m.chat, sfoto, 'error.jpg', res, m)
 } catch {
 await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*')}}
 handler.command = ['stickersearch2', 'searchsticker2', 'stickerssearch2', 'searchstickers2']
-handler.register = true
 handler.limit = 2
 export default handler
